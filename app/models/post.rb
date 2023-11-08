3a3ad1f2d58ce253668fb8133b3de7c11f4db5ef
Megan <esler6724@gmail.com>
2023-11-07 22:37:06 -0800

    commit tree 15f68a1066cb2b1fef6fbe05425745ab28b20e64

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :title_validator
+
+    def title_validator
+        clickbait_strings = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        unless clickbait_strings.any? { |string| title&.include?(string)}
+            errors.add(:title, "must include clickbait")
+        end 
+    end
+
 end
